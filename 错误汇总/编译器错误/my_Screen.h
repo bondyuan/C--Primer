ff30b0a01c8c61d953c384fee059587f0a93a5cd
bondyuan <yuanbo.1990@gmail.com>
2014-11-11 11:23:33 +0800

    VS2013编译器错误
    在g++ -std=c++11 中编译 无问题

---
+++ 错误汇总/编译器错误/my_Screen.h
@@ -0,0 +1,59 @@
+﻿#ifndef _MY_SCREEN_H_
+#define _MY_SCREEN_H_
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Screen
+{
+	typedef unsigned int uint;
+public:
+	friend class Window_mgr;
+	Screen() = default;
+	Screen(uint wd, uint hg) : width(wd), height(hg), content(wd * hg, ' ') {}
+	Screen(uint wd, uint hg, char a) : width(wd), height(hg), content(wd * hg, a) {}
+	Screen &set(char);
+	Screen &set(uint, uint, char);
+	Screen &display(ostream &os) {do_display(os); return *this;}
+	const Screen &display(ostream &os) const {do_display(os); return *this;}
+	Screen &move(uint, uint);
+	char get(uint, uint);
+private:
+	uint width = 0, height = 0, cursor = 0;
+	string content;
+	void do_display(ostream &os) const;
+};
+
+inline Screen &Screen::set(char c)
+{
+	content[cursor] = c;
+	return *this;
+}
+inline Screen &Screen::set(uint wd, uint hg, char ch)
+{
+	content[wd*width+hg] = ch;
+	return *this;
+}	
+inline Screen &Screen::move(uint r, uint c)
+{
+	uint row = (r-1) * width;
+	cursor = row + c;
+	return *this;
+}
+inline char Screen::get(uint r, uint c)
+{
+	uint row = r * width;
+	return content[row + c];
+}
+void Screen::do_display(ostream &os) const 
+{
+	for (uint i = 0; i < height; ++i)
+	{
+		for (uint j = 0; j < width; ++j)
+			os << content[i * width + j];
+		os << endl;
+	}
+	os << "The current cursor is " << cursor << endl;
+}
+
+#endif
---
+++ 错误汇总/编译器错误/my_Windowmgr.h
@@ -0,0 +1,37 @@
+﻿#ifndef _MY_WINDOWMGR_H_
+#define _MY_WINDOWMGR_H_
+#include <vector>
+#include "my_Screen.h"
+
+using namespace std;
+//class Screen;
+class Window_mgr
+{
+	Window_mgr()
+	{
+		screens.push_back(Screen(3, 10, 'a')); 	//如果改成screens{Screen(3, 10, 'a')}就会报错
+	}
+	Window_mgr(char a)
+	{
+		screens{Screen(3, 10, a)};		//如果改成screens{Screen(3, 10, 'a')}就会报错
+	}
+public:
+	typedef vector<Screen>::size_type ScreenIndex;
+	void clear(ScreenIndex);
+	unsigned char addScreen(const Screen&);
+private:
+	vector<Screen> screens;				//如果改成screens{Screen(3, 10, 'a')}就会报错
+};
+void Window_mgr::clear(ScreenIndex i)
+{
+	Screen &s = screens[i];
+	s.content = string(s.height*s.width, ' ');
+}
+
+unsigned char Window_mgr::addScreen(const Screen &s)
+{
+	screens.push_back(s);
+	return screens.size()-1;
+}
+
+#endif
